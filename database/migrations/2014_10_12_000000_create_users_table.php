ace161bf1116986aaff8e1caa0c9517803b99908
SophanonChhoun <sophanon.chhoun@niptict.edu.kh>
2021-04-16 17:34:56 +0700

    [IMP]user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->bigInteger("media_id");
+            $table->bigInteger("media_id")->nullable();
             $table->tinyInteger("is_enable");
             $table->string('password');
             $table->timestamps();
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use DB;
 
 class UserSeeder extends Seeder
 {
